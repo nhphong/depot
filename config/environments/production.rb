29f0b9a9558e3f8d0458a5c99867620fe5753cb8
Phong Nguyen <phong.nguyen@eastagile.com>
2015-04-09 17:46:42 +0700

    Finished Chapter 8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Turn on caching
+  config.action_controller.perform_caching = false
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,10 @@ Product.create!(title: 'CoffeeScript',
 Product.create!(title: 'Programming Ruby 1.9 & 2.0',
                 description:
                        %{<p>
+                          <b>
                           Ruby is the fastest growing and most exciting dynamic language
                           out there. If you need to get working programs delivered fast,
-                          you should add Ruby to your toolbox.
+                          you should add Ruby to your toolbox.</b>
                         </p>},
                 image_url: 'ruby.jpg',
                 price: 49.95)
